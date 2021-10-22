fe187f4588d5741fab6ee3e91cdf55a39f3eb8de
PedroMarineli <phmo.1606@gmail.com>
2021-10-22 10:42:05 +0000

    gg

--- index.php
+++ index.php
@@ -8,20 +8,12 @@ $comando = $bd->prepare('SELECT * FROM generos');
 $comando->execute();
 $generos = $comando->fetchAll(PDO::FETCH_ASSOC);
 
+$_title = 'Gêneros';
+
 ?>
 
+<?php include('./includes/header.php'); ?>
 
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>biblioteca</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
-</head>
-<body>
-    <main class="container">
         <a class="btn btn-primary" href="insert.php">Novo Gênero</a>
         <table class="table">
             <tr>
