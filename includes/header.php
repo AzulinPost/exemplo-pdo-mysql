7f0279ba10cbd8597210b1f180ead1eef82eb974
PedroMarineli <phmo.1606@gmail.com>
2021-10-22 11:14:22 +0000

    ytyt

--- includes/header.php
+++ includes/header.php
@@ -6,4 +6,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.php">Biblioteca</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        </div>
+
+    </nav>
     <main class="container">
