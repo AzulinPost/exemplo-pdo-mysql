3a2584159328b135753a9583d838adb7ef6850ed
PedroMarineli <phmo.1606@gmail.com>
2021-10-22 10:51:07 +0000

    ggg

--- insert.php
+++ insert.php
@@ -12,17 +12,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     header('Location:/index.php');
 }
 
+
+$_title = ' Novo Gênero';
+
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-</head>
-<body>
-    <main class="container">
+<?php include('./includes/header.php'); ?>
         <h1>Novo Gênero</h1>
         <form action="insert.php" method="post">
             <div class="form-group">
